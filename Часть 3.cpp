fd839eb925e058da95cadc9a23e043e6f16cf2cb
Artem292 <60403062+Artem292@users.noreply.github.com>
2023-11-15 11:04:26 +0300

    Add files via upload

---
+++ Часть 3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = { 92,24,15,8 };
+    int s = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        s *= arr[i];
+    }
+    cout << s;
+}
