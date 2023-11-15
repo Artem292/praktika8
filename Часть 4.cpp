310f817341022036784feb3d1cfabcfd2db954e6
Artem292 <60403062+Artem292@users.noreply.github.com>
2023-11-15 11:14:01 +0300

    Add files via upload

---
+++ Часть 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int num;
+	cout << " введите число: ";
+	cin >> num;
+	if (num == 0)
+	{
+		cout << " число равно 0 " << endl;
+	}
+	if (num > 0)
+	{
+		cout << " число положительное " << endl;
+	}
+	if (num < 0)
+	{
+		cout << " число отрицательное " << endl;
+	}
+	 return 0;
+}
